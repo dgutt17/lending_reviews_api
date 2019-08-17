afeba378876198c26c35e4a83155407fd16c6808
Dan Gutt <dgutt10@gmail.com>
2019-08-16 21:52:11 -0500

    Made code in parser less complicated

--- lib/lending_tree_parser.rb
+++ lib/lending_tree_parser.rb
@@ -2,34 +2,48 @@ require 'nokogiri'
 require 'open-uri'
 
 class LendingTreeParser
-    attr_accessor :url, :reviews, :doc
+    attr_accessor :url, :reviews, :doc, :lender_nav_present
 
     def initialize(url)
         @url = url
-        @initial_doc = Nokogiri::HTML(open(url))
+        # @initial_doc = Nokogiri::HTML(open(url))
         @stored_reviews = []
+        @thread_array = []
+        @query_string = "?sort=cmV2aWV3c3VibWl0dGVkX2Rlc2M=&pid="
+        @lender_nav_present = true
     end
 
     def page_iterator
-        index = 0
-        reviews = @initial_doc.css(".lenderReviews")
-        links = reviews.css(".page-link")
+        page_num = 1
+        # reviews = @initial_doc.css(".lenderReviews")
+        # lender_nav = reviews.css(".lenderNav.pagination")
+        # links = lender_nav.css(".page-link")
+        url_hash = {}
         
         while true do
-            if index == links.count - 1
-                if links[links.count - 1].content == "Next"
-                    doc = Nokogiri::HTML(open(links[links.count - 2].attributes["href"].content))
-                    reviews = doc.css(".lenderReviews")
-                    links = reviews.css(".page-link")
-                    index = 0
-                else
-                    break
-                end
-            end
+            puts "top of loop"
+            # if index == links.count - 1
+            #     if links[links.count - 1].content == "Next"
+            #         doc = Nokogiri::HTML(open(links[links.count - 2].attributes["href"].content))
+            #         lender_nav = doc.css(".lenderNav.pagination")
+            #         links = lender_nav.css(".page-link")
+            #         index = 0
+            #     else
+            #         html_doc_iterator(links[index].attributes["href"].content)
+            #         break
+            #     end
+            # end
 
-            html_doc_iterator(links[index].attributes["href"].content)
+            
+            # if !url_hash[links[index].attributes["href"].content]
+            #     url_hash[links[index].attributes["href"].content] = true
+            puts "page_num: #{page_num}"
+                @thread_array << Thread.new {html_doc_iterator("#{@url}#{@query_string}#{page_num}")}
+            # end
 
-            index += 1
+            break if !@lender_nav_present 
+
+            page_num += 1
         end
 
         return @stored_reviews
@@ -40,6 +54,8 @@ class LendingTreeParser
     def html_doc_iterator(url)
         puts "url: #{url}"
         doc = Nokogiri::HTML(open(url))
+        @lender_nav_present = doc.css(".lenderNav.pagination").present?
+        return if !@lender_nav_present
         reviews = doc.css(".lenderReviews")
         reviews.css(".mainReviews").each do |elem|
             hash = {}
