07a667e809e3400428d94ead82592d35e5731e5c
Dan Gutt <dgutt10@gmail.com>
2019-08-19 00:24:16 -0500

    db/schema update

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,6 @@ ActiveRecord::Schema.define(version: 2019_08_17_210800) do
     t.string "url"
   end
 
-  create_table "businesses", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "content"
